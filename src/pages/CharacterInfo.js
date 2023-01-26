757f72395c4216c0cf4fb924b9a62a7b41e9c577
joyce <joycemendes.jm79@gmail.com>
2023-01-26 15:42:10 -0300

    consumindo dados do hook e api

--- src/pages/CharacterInfo.js
+++ src/pages/CharacterInfo.js
@@ -1,8 +1,26 @@
 import React from 'react'
+import CharacterCard from '../components/CharacterCard';
+import { useCharacter } from '../hooks/useCharacter'
+
 
 const CharacterInfo = () => {
+
+  const { characterInfo } = useCharacter();
+
   return (
-    <div>CharacterInfo</div>
+
+    <div>
+       { characterInfo && (
+        <>
+          <CharacterCard character={characterInfo}/>
+          <p><span>Status: </span>{characterInfo.status}</p>
+          <p><span>Specie: </span>{characterInfo.species}</p>
+          <p><span>Gender: </span>{characterInfo.gender}</p>
+          <p><span>Origin: </span>{characterInfo.origin.name}</p> 
+          <p><span>Location: </span>{characterInfo.location.name}</p>
+        </>
+       )}
+    </div>
   )
 }
 
