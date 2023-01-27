e2227b35ce98acad5f6d026138b6bb9a0d5a5503
joyce <joycemendes.jm79@gmail.com>
2023-01-27 14:45:06 -0300

    navbar

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,22 +1,55 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import { useState } from "react"
+import { Link, useNavigate } from 'react-router-dom'
 
 //css
 import  styles from "./Navbar.module.css";
 
-const Navbar = () => {
-  return (
-    <nav className={styles.navbar}>
+//icons
+import { BiSearchAlt2 } from "react-icons/bi";
 
-        <Link to={"/"} className={styles.logo}>
-          <h1>Ricky and Morty</h1>
-        </Link>
-        
-        <div className={styles.navbar_link}>
-          <Link to={"/"}>Characters</Link>
-          <Link to={"/location"}>Locations</Link>
-        </div>
-    </nav>
+const Navbar = () => {
+
+  const [search, setSearch] = useState();
+  const navigate = useNavigate();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    if(!search) return;
+
+    navigate(`/search?q=${search}`)
+
+    setSearch("");
+
+  }
+  
+  return (
+
+    <div className={styles.navbar_container}>
+        <nav className={styles.navbar}>
+
+            <Link to={"/"} className={styles.logo}>
+              <h1>Ricky and Morty</h1>
+            </Link>
+
+            <form onSubmit={handleSubmit} className={styles.form}>
+
+              <input type="text" 
+              placeholder="Search a character" 
+              className={styles.input_text} 
+              onChange={(e) => setSearch(e.target.value)}
+              value={search}/>
+
+            <button type="submit" className={styles.btn}>
+                <BiSearchAlt2/>
+            </button>
+            </form> 
+        </nav>
+    <div className={styles.navbar_links}>
+        <Link to="/">Characters</Link>
+        <Link to="/location">Locations</Link>
+    </div>
+  </div>
   )
 }
 
