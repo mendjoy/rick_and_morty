57cb18c5c0718398c6987d555f76fecba571de00
joyce <joycemendes.jm79@gmail.com>
2023-01-30 10:33:34 -0300

    paginação episodios

--- src/hooks/useFetchEpisodes.js
+++ src/hooks/useFetchEpisodes.js
@@ -3,21 +3,34 @@ import { useState, useEffect } from "react"
  export const useFetchEpisodes = () => {
 
     const [episodes, setEpisodes] = useState([]);
+    const [info, setInfo] = useState({})
 
     const url = "https://rickandmortyapi.com/api/episode";
 
     const fetchDataEpisodes = (url) => {
         fetch(url)
             .then(response => response.json())
-            .then(data => setEpisodes(data.results))
+            .then(data => {
+                setInfo(data.info);
+                setEpisodes(data.results);
+            })
             .catch(err => console.log(err))
     };
+
+    const onPrevious = () => {
+        fetchDataEpisodes(info.prev)
+       }
+    
+       const onNext = () => {
+        fetchDataEpisodes(info.next)
+       }
+    
 
     useEffect(() => {
         fetchDataEpisodes(url);
         
     },[])
 
-   return { episodes }
+   return { episodes, info, onPrevious, onNext }
   
 }
---
+++ src/pages/Episodes/Episodes.js
@@ -0,0 +1,42 @@
+//components
+import EpisodesCard from "../../components/EpisodesCard/EpisodesCard";
+import Pagination from "../../components/Pagination/Pagination";
+
+//hooks
+import { useFetchEpisodes } from "../../hooks/useFetchEpisodes";
+
+//css
+import styles from "./Episodes.module.css"
+
+
+const Episodes = () => {
+
+  const { episodes, info, onNext, onPrevious } = useFetchEpisodes();
+
+  return (
+    <div>
+      <Pagination 
+        prev={info.prev} 
+        next={info.next} 
+        onPrevious={onPrevious} 
+        onNext={onNext}/>
+
+
+        <div className={styles.episodes_container}>
+          { episodes.length === 0 && <p>Carregando...</p>}
+          { episodes.length > 0 && episodes.map((episode) => 
+          <EpisodesCard key={episode.id} 
+          episode={episode}/>)}
+          
+        </div>
+
+        <Pagination 
+        prev={info.prev} 
+        next={info.next} 
+        onPrevious={onPrevious} 
+        onNext={onNext}/>
+    </div>
+  )
+}
+
+export default Episodes
